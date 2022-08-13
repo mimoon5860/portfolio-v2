0c24208c54b782056e43c3433a01d17e52125307
mimoon5860 <mahmodulislammoon@gmail.com>
2022-08-13 16:39:45 +0600

    home almost done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import './Home.css'
 import Fade from 'react-reveal/Fade'
-import { Bounce } from 'react-reveal'
 import { Link } from 'react-scroll'
 import Particles from 'react-particles-js'
 import Typewriter from 'typewriter-effect'
 import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle'
 import Navbar from '../navbar/Navbar'
 import config from '../../config'
-import profile from '../../images/matt.png'
+import profile from '../../images/moondp.jpg'
 import linkedin from '../../images/social/linkedin.png'
+import facebook from '../../images/social/facebook.png'
+import github from '../../images/social/github.png'
 
 const Home = () => {
   const [imageLoaded, setImageLoaded] = useState(false)
@@ -17,64 +18,91 @@ const Home = () => {
     <div className="home-wrapper">
       <div className="home">
         <Particles className="particles" params={config.particles} />
-        <div className={`greeting${!imageLoaded ? ' hide' : ''}`}>
-          <Fade bottom distance="40px">
-            <img
-              className="profile"
-              alt="m.jigalin profile"
-              src={profile}
-              onLoad={() => setImageLoaded(true)}
-            />
-            <h1 className="greeting-text">
-              Hi, I'm <span className="name">Md Mahmudul islam Moon</span>.{' '}
-              <span className="wave-emoji" role="img" aria-label="waving hand">
-                👋
-              </span>
-            </h1>
-            <h1 className="greeting-text">
-              <Typewriter
-                options={{
-                  strings: [
-                    'I like to design things.',
-                    'I love learning new tech.',
-                    'I love meeting new people.',
-                    'I create unique digital experiences.',
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  deleteSpeed: 10,
-                  cursor: '<',
-                  delay: 100,
-                }}
+        <div className={`greeting${!imageLoaded ? 'hide' : ''}`}>
+          <Fade bottom distance='40px'>
+            <div className='img-and-logos' >
+              <img
+                className="profile"
+                alt="Mahmudul islam moon profile"
+                src={profile}
+                onLoad={() => setImageLoaded(true)}
               />
-            </h1>
-            <Bounce cascade>
-              <div className="links">
-                <a
-                  href="https://www.linkedin.com/in/mjigalin/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={linkedin} alt="Linkedin Logo" width="50px" />
-                </a>
+              <div className="social-logos">
+                <div className="links">
+                  <a
+                    href="https://www.linkedin.com/in/mahmudul-islam-moon/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={linkedin} alt="Linkedin Logo" width="50px" />
+                  </a>
+                </div>
+                <div className="links">
+                  <a
+                    href="https://www.facebook.com/em.ai.moon"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={facebook} alt="Facebook Logo" width="50px" />
+                  </a>
+                </div>
+                <div className="links">
+                  <a
+                    href="https://github.com/mimoon5860"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={github} alt="Github Logo" width="50px" />
+                  </a>
+                </div>
               </div>
-            </Bounce>
-            <div className="scroll-down">
-              <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-63}
-                duration={500}
-              >
-                <ArrowDropDownCircleIcon
-                  fontSize="large"
-                  style={{ pointerEvents: 'fill', cursor: 'pointer' }}
-                />
-              </Link>
             </div>
           </Fade>
+        </div>
+        <div className={`greeting${!imageLoaded ? ' hide' : ''}`}>
+          <Fade bottom distance="40px">
+            <div style={{ marginTop: '-200px' }}>
+              <h1 className="greeting-text">
+                Hi, I'm <span className="name">Moon</span>.{' '}
+                <span className="wave-emoji" role="img" aria-label="waving hand">
+                  👋
+                </span>
+              </h1>
+              <h1 className="greeting-text">
+                <Typewriter
+                  options={{
+                    strings: [
+                      '<I like to develop things.',
+                      '<I love learning new tech.',
+                      '<I love meeting new people.',
+                      '<I create unique digital experiences.',
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 10,
+                    cursor: '/>',
+                    delay: 100,
+                  }}
+                />
+              </h1>
+              <a className='download-resume' target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1I7hTDlndoiyMn1mR3Vf0N6PjHg8EiULq/view">Download Resume</a>
+            </div>
+          </Fade>
+        </div>
+        <div className="scroll-down">
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-63}
+            duration={500}
+          >
+            <ArrowDropDownCircleIcon
+              fontSize="large"
+              style={{ pointerEvents: 'fill', cursor: 'pointer' }}
+            />
+          </Link>
         </div>
         <Navbar />
       </div>
